a4dbdda71348432453231c32c5e3b1327f0520fe
saksham123 <sakshmrana@gmail.com>
2019-02-26 21:38:11 -0500

    Add files via upload

---
+++ Create dbo.new_table_1.sql
@@ -0,0 +1,18 @@
+﻿USE [New Database]
+GO
+
+/****** Object: Table [dbo].[new_table] Script Date: 2/26/2019 9:36:25 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[new_table] (
+    [Productid] NVARCHAR (50) NULL,
+    [Sales]     INT           NULL,
+    [Product]   VARCHAR (50)  NULL,
+    [SalesID]   INT           NOT NULL
+);
+
+
---
+++ Create dbo.old_table_1.sql
@@ -0,0 +1,18 @@
+﻿USE [New Database]
+GO
+
+/****** Object: Table [dbo].[old table] Script Date: 2/26/2019 9:36:47 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[old table] (
+    [Productid] NVARCHAR (50) NOT NULL,
+    [Ranking]   INT           NULL,
+    [Product]   NCHAR (10)    NULL,
+    [sales]     INT           NULL
+);
+
+
